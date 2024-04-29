1f174851679789e3b67b18b7e713adaa228a3edd
Dinanath Shah <159602182+GitNath06@users.noreply.github.com>
2024-04-29 18:20:16 +0545

    Add files via upload

---
+++ pattern_1.java
@@ -0,0 +1,24 @@
+import java.io.*;
+public class pattern_1 {
+    public static void main(String[] args){
+
+   //solid pattern
+   /*
+
+    *****
+    *****
+    *****
+    *****
+
+    */
+    
+    for(int i=0; i<=3; i++){
+        for(int j=0; j<=5;j++){
+            System.out.print("@ ");
+        }
+        System.out.println("\n");
+    }
+
+    }
+    
+}
